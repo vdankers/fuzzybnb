cf3ae6a1827197c41cf8cc5b561c315f579bbb88
Regenworm Desktop Mac <alexkhawalid@iMac-van-Alex.local>
2016-12-16 19:54:11 +0100

    CV workspaces
    HYFIS parameter test set up and resulting workspaces for WM parameter
    tests available in RWorkspaces

--- RLearn/learnANFIS.R
+++ RLearn/learnANFIS.R
@@ -1,24 +1,26 @@
 # type of method
 method.type <- "ANFIS"
 
-# List of parameters for the prediction engine
+
+# a list containing parameters frbs.learn
 control <- list(num.labels = 5, max.iter = 100, step.size = 0.1,
   type.tnorm = "PRODUCT", type.implication.func = "DIENES_RESHER", name = "fuzzybnbANFIS")
 
-# Get frbs object
+# Make frbs object
 object.reg <- frbs.learn(data.train, range.data, method.type, control)
 
 # show membership functions
 pdf("Rplots/ANFISMF.pdf")
 par("mar")
-
 par(mar=c(1,1,1,1))
 plotMF(object.reg)
 dev.off()
 
 # test
-res.test <- predict(object.reg, data.test)
+res.cv <- predict(object.reg, data.test)
 
 # show error
-error = sum(abs(res.test - data.targets))/length(data.targets)
+error = sum(abs(res.cv - data.targets))/length(data.targets)
 print(error)
+
+save.image(file="/RWorkspaces/ANFIS")
--- RLearn/learnHYFIS.R
+++ RLearn/learnHYFIS.R
@@ -1,14 +1,20 @@
 # type of method
 method.type <- "HYFIS"
 
-# a list containing all parameters
+
+# reduce inputs for HYFIS and ANFIS
+data.train <- cbind(data.train[,1:num_inps],prices)
+data.test <- data.test[,1:num_inps]
+
+
+
+# a list with all parameters
 control <- list(num.labels = 5, max.iter = 100, step.size = 0.1,
   type.tnorm = "PRODUCT", type.defuz = "COG", type.implication.func = "DIENES_RESHER",
   name = "fuzzybnbHYFIS")
 
 
-# Get frbs object
-object.reg <- frbs.learn(data.train, range.data, method.type, control)
+object.reg <- frbs.learn(data.train, range.data[,1:num_inps], method.type, control)
 
 # show membership functions
 pdf("Rplots/HYFISMF.pdf")
@@ -18,8 +24,10 @@ plotMF(object.reg)
 dev.off()
 
 # test
-res.test <- predict(object.reg, data.test)
+res.cv <- predict(object.reg, data.cv)
 
 # show error
-error = sum(abs(res.test - data.targets))/length(data.targets)
+error = sum(abs(res.cv - data.cvtargets))/length(data.cvtargets)
 print(error)
+
+save.image(file="HYFIS")
